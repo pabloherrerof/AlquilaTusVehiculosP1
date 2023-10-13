a381637bd9b7a32dc85857361702434a494630c7
Pablo Herrero Ferrán <120820455+pabloherrerof@users.noreply.github.com>
2023-10-13 09:28:39 +0200

    first commit

---
+++ src/main/java/com/AlquilaTusVehiculosP1/AlquilaTusVehiculos/AlquilaTusVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.AlquilaTusVehiculosP1.AlquilaTusVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaTusVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaTusVehiculosApplication.class, args);
+	}
+
+}
